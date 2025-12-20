25157fa57a446564abab59a1073abccc4a1ac4f1
Camila Bouchon <cami.bouchon@gmail.com>
2025-12-20 21:31:55 +0000

    added sales_margin table

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,37 @@
+WITH 
+
+sales AS (
+    SELECT 
+        date_date, 
+        orders_id, 
+        products_id, 
+        revenue, 
+        quantity
+    FROM {{ ref('stg_raw__sales')}}
+    ),
+
+products AS (
+    SELECT
+        products_id,
+        purchase_price
+    FROM {{ ref('stg_raw__product')}} 
+    ),
+sales_margin AS (
+    SELECT 
+        s.date_date, 
+        s.orders_id, 
+        s.products_id, 
+        s.revenue, 
+        s.quantity,
+        p.purchase_price,
+
+        --metrics--
+        p.purchase_price * s.quantity AS purchase_cost,
+        s.revenue - (p.purchase_price * s.quantity) AS margin
+
+    FROM sales s
+    LEFT JOIN products p
+    USING (products_id)
+)
+SELECT *
+FROM sales_margin
